4a32f2a0134f9c64a2f1d72df00ae76a82486840
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-21 19:21:21 +0530

    All Components Added

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,69 @@
-import React, {Component, useState} from "react";
-import slides from './data.js';
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
-const App = () => {
+const App = (props) => {
+  const [slideIndex, setSlideIndex] = useState(0);
+  const [slide, setSlide] = useState(props.slides[0]);
+  const [prev, setPrev] = useState(true);
+  const [next, setNext] = useState(false);
+  const [restart, setRestart] = useState(true);
 
-  const [currentSlide, setCurrentSlide] = useState(1);
-  
-  function previous() {
-    if(currentSlide > 1) {
-      setCurrentSlide(currentSlide - 1);
+  function nextSlide() {
+    setSlideIndex(slideIndex + 1);
+    setSlide(props.slides[slideIndex + 1]);
+    if (slideIndex === 3) {
+      setNext(true);
+    }
+    if (slideIndex > -1) {
+      setPrev(false);
+      setRestart(false);
+    }
+  }
+  function restartApp() {
+    setSlideIndex(0);
+    setNext(false);
+    setPrev(true);
+    setRestart(true);
+    setSlide(props.slides[0]);
+  }
+  function prevSlide() {
+    setSlideIndex(slideIndex - 1);
+    setSlide(props.slides[slideIndex - 1]);
+    setNext(false);
+    if (slideIndex === 1) {
+      setPrev(true);
+      setRestart(true);
+      //setNext(false);
     }
   }
 
-  function next() {
-    
-  }
-
-  function restart() {
-    
-  }
-  
   return (
     <>
-      <div>{slides[0]}</div>;
-      <button data-testid="button-prev" onClick="previous()" disabled>Previous</button>
-      <button data-testid="button-restart" onClick="restart()" disabled>Restart</button>
-      <button data-testid="button-next" onClick="next()">Next</button>
+      <h1 data-testid="title">{slide.title}</h1>
+      <p data-testid="text">{slide.text}</p>
+      <button
+        data-testid="button-restart"
+        onClick={() => restartApp()}
+        disabled={restart}
+      >
+        Restart
+      </button>
+      <button
+        data-testid="button-prev"
+        onClick={() => prevSlide()}
+        disabled={prev}
+      >
+        Prev
+      </button>
+      <button
+        data-testid="button-next"
+        onClick={() => nextSlide()}
+        disabled={next}
+      >
+        Next
+      </button>
     </>
-  )
-}
-
+  );
+};
 
 export default App;
