726858a48cca32b3b164e9121c3f822937aa618a
abhishekshaw1007 <abhishekshaw1007@gmail.com>
2020-11-21 18:00:21 +0530

    Added buttons and data-testid

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,32 @@
 import React, {Component, useState} from "react";
+import slides from './data.js';
 import '../styles/App.css';
 
 const App = () => {
+
+  const [currentSlide, setCurrentSlide] = useState(1);
+  
+  function previous() {
+    if(currentSlide > 1) {
+      setCurrentSlide(currentSlide - 1);
+    }
+  }
+
+  function next() {
+    
+  }
+
+  function restart() {
+    
+  }
+  
   return (
-    <></>
+    <>
+      <div>{slides[0]}</div>;
+      <button data-testid="button-prev" onClick="previous()" disabled>Previous</button>
+      <button data-testid="button-restart" onClick="restart()" disabled>Restart</button>
+      <button data-testid="button-next" onClick="next()">Next</button>
+    </>
   )
 }
 
